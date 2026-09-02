10f59ed8364b20840d134f322bb906b10a1b10ba
Sonu Kumar <gautamrocky909621@gmail.com>
2026-09-02 19:10:30 +0000

    done wiht the iitial setup of the project

---
+++ langchian4j/src/main/java/cs/sonu/langchain4j/Langchian4jApplication.java
@@ -0,0 +1,13 @@
+package cs.sonu.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchian4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchian4jApplication.class, args);
+	}
+
+}
---
+++ langchian4j/src/test/java/cs/sonu/langchain4j/Langchian4jApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.sonu.langchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchian4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
